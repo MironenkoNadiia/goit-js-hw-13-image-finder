8ebd9daae69d23a8fec415cc4412ea4ffbd53a77
MironenkoNadiia <candleflame@i.ua>
2020-12-01 19:51:56 +0200

    almost

--- goit-js-hw-13-image-finder/src/index.js
+++ goit-js-hw-13-image-finder/src/index.js
@@ -1,1 +1,64 @@
 import './styles.css';
+import debounce from 'lodash.debounce';
+
+import imagesCardTpl from './templates/img-card-markup.hbs';
+import PixabayApiServise from './js/APIService';
+import { onClickImage } from './js/modal-img';
+
+
+
+const refs = {
+  input: document.querySelector('#search-form input'),
+  outputList: document.querySelector('.gallery'),
+  watcher: document.querySelector('.watcher'),
+};
+
+const pixabayApiServise = new PixabayApiServise();
+
+refs.input.addEventListener('input', debounce(onSearchInputChange, 500));
+refs.outputList.addEventListener('click', onClickImage);
+
+function onSearchInputChange({ target }) {
+  pixabayApiServise.query = target.value;
+  if (pixabayApiServise.query === '') {
+    clearArticlesList();
+    return;
+  }
+
+  clearArticlesList();
+  pixabayApiServise.resetPage();
+  fetchToPixabayApiAndRender();
+}
+
+function fetchToPixabayApiAndRender() {
+  pixabayApiServise.fetchImages().then(appendImagesMarkup);
+}
+
+function appendImagesMarkup({ hits }) {
+  refs.outputList.insertAdjacentHTML('beforeend', imagesCardTpl(hits));
+}
+
+function clearArticlesList() {
+  refs.outputList.innerHTML = '';
+}
+
+//infinite scroll settings
+
+const intersectionCallback = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && pixabayApiServise.query !== '') {
+      fetchToPixabayApiAndRender();
+    }
+  });
+};
+
+const intersectionOptions = {
+  rootMargin: '0% 0% 50% 0%',
+};
+
+const observer = new IntersectionObserver(
+  intersectionCallback,
+  intersectionOptions,
+);
+
+// observer.observe(refs.watcher);
